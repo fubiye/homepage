89b91c7e4274d95769b6b9ab64f6d27d8402049b
Fu, Biye <biye.fu@outlook.com>
2020-04-05 22:37:54 +0800

    add link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NzIconService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'homepage';
+  constructor(private iconService: NzIconService){
+    this.iconService.fetchFromIconfont({
+      scriptUrl: 'https://at.alicdn.com/t/font_8d5l8fzk5b87iudi.js'
+    });
+  }
 }
